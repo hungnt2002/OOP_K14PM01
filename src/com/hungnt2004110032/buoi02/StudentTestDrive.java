e545dfc04b449c3e05304eb4a60b419b189a988b
hungnt2002 <hung0983330069@gmailcom>
2022-01-13 17:16:05 +0700

    nop bai student buoi 2

--- src/com/hungnt2004110032/buoi02/StudentTestDrive.java
+++ src/com/hungnt2004110032/buoi02/StudentTestDrive.java
@@ -2,18 +2,22 @@ package com.hungnt2004110032.buoi02;
 
 public class StudentTestDrive {
     public static void main(String[] args) {
-        Student student;
-        student = new Student("Nguyen Minh Tuan", 18, "Nam", "CNTT", 1.5);
-        student.hoc();
-        student.thi();
-        student.dangKyMonHoc();
-        student = new Student("Le Viet Duc", 18, "Nam", "CNTT", 2.0);
-        student.hoc();
-        student.thi();
-        student.dangKyMonHoc();
-        student = new Student("Nguyen Truong Hung", 18, "Nam", "CNTT", 2.5);
-        student.hoc();
-        student.thi();
-        student.dangKyMonHoc();
+
+        Student tuanStudent = new Student("Nguyen Minh Tuan", 18, "Nam", "CNTT", 1.5);
+        tuanStudent.hoc();
+        tuanStudent.thi();
+        tuanStudent.dangKyMonHoc();
+
+        System.out.println("------------------------------");
+        Student ducStudent = new Student("Le Viet Duc", 19, "Nam", "CNTT", 2.0);
+        ducStudent.hoc();
+        ducStudent.thi();
+        ducStudent.dangKyMonHoc();
+
+        System.out.println("------------------------------");
+        Student hungStudent = new Student("Nguyen Truong Hung", 20, "Nam", "CNTT", 2.5);
+        hungStudent.hoc();
+        hungStudent.thi();
+        hungStudent.dangKyMonHoc();
     }
 }
