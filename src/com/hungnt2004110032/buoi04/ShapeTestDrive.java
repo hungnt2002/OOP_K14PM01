44122ee0983aa5c1b375e46c9bf38364e4919f1b
hungnt2002 <hung0983330069@gmailcom>
2022-01-20 15:11:42 +0700

    bài tập xoay hình (cập nhật thêm hình amoeba)

--- src/com/hungnt2004110032/buoi04/ShapeTestDrive.java
+++ src/com/hungnt2004110032/buoi04/ShapeTestDrive.java
@@ -2,16 +2,19 @@ package com.hungnt2004110032.buoi04;
 
 public class ShapeTestDrive {
     public static void main(String[] args) {
-        Circle circle = new Circle("hinhtron.AIF");
+        Circle circle = new Circle("hinhtron.aif");
         circle.rotate();
         circle.playSound();
 
-        Square square = new Square("hinhvuong.AIF");
+        Square square = new Square("hinhvuong.aif");
         square.rotate();
         square.playSound();
 
-        Triangle triangle = new Triangle("hinhtamgiac.AIF");
+        Triangle triangle = new Triangle("hinhtamgiac.aif");
         triangle.rotate();
         triangle.playSound();
+
+        Amoeba amoeba = new Amoeba("amoebaMusic.hif");
+        amoeba.rotate();
     }
 }
