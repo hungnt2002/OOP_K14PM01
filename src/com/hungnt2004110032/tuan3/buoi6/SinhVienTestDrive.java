039fbf44b410301e55439624cc57c0f875654e21
hungnt2002 <hung0983330069@gmailcom>
2022-02-24 14:50:34 +0700

    up bài Sinh viên tuần 3 buổi 6

---
+++ src/com/hungnt2004110032/tuan3/buoi6/SinhVienTestDrive.java
@@ -0,0 +1,17 @@
+package com.hungnt2004110032.tuan3.buoi6;
+
+import java.util.Scanner;
+
+public class SinhVienTestDrive {
+    
+    public static void main(String[] args) {
+
+        SinhVien sinhVien1 = new SinhVien(2004110032, "Nguyen Truong Hung", 6, 7.5);      
+        sinhVien1.xuatThongTin();
+        
+        SinhVien sinhVien2 = new SinhVien(2004110047, "Le Hoang Gia Bao", 7, 8.5);      
+        sinhVien2.xuatThongTin();
+
+
+    }
+}
