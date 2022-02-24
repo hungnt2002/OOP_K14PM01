8b2cb14e3ab82f12f1fcf8ef1ce94259cbca8586
hungnt2002 <hung0983330069@gmailcom>
2022-02-24 11:44:19 +0700

    Lab 4 bài 3

--- src/com/hungnt2004110032/buoi04/lab4/SanPhamTestDrive.java
+++ src/com/hungnt2004110032/buoi04/lab4/SanPhamTestDrive.java
@@ -4,15 +4,19 @@ public class SanPhamTestDrive {
     
     public static void main(String[] args) {
 
-        System.out.println("- San pham doi tuong 1");
+        System.out.println("- San pham doi tuong 1"); // Bài 1
         SanPham sanPham1 = new SanPham();
         sanPham1.nhap();
         sanPham1.xuat();
 
-        System.out.println("- San pham doi tuong 2");
+        System.out.println("- San pham doi tuong 2"); // Bài 2
         SanPham sanPham2 = new SanPham();
         sanPham2.nhap();
         sanPham2.xuat();
+
+        System.out.println("- San pham doi tuong 3 (su dung ham tao)"); // bài 3
+        SanPham sanPham3 = new SanPham("Laptop Dell", 12500000);
+        sanPham3.xuat();
         
     }
 }
